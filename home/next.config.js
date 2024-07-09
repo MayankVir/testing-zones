7678bef568de99adb901b5adafa836ac10bda657
MayankVir <virmanimayank15@gmail.com>
2024-07-10 04:32:48 +0530

    added new config

--- home/next.config.js
+++ home/next.config.js
@@ -18,13 +18,13 @@ const nextConfig = {
       },
     ];
   },
-  redirects: [
-    {
-      source: "/blog/",
-      destination: "/blog",
-      permanent: false,
-    },
-  ],
+  // redirects: [
+  //   {
+  //     source: "/blog/",
+  //     destination: "/blog",
+  //     permanent: false,
+  //   },
+  // ],
 };
 
 module.exports = nextConfig;
